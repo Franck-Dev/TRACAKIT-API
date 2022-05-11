768546d3d9a4b027b5b43bf00774f14993fbb0da
DARTOIS Franck <f.dartois@daher.com>
2022-05-11 12:10:17 +0200

    Rajout filtres dans Ress Molding

--- src/Entity/Molding.php
+++ src/Entity/Molding.php
@@ -4,11 +4,14 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\MoldingRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ApiResource(
@@ -21,7 +24,15 @@ use Symfony\Component\Validator\Constraints as Assert;
  *      itemOperations={"get","put"={"security"="is_granted('ROLE_CONTROLEUR')"},
  *                      "patch"={"security"="is_granted('ROLE_USER')"},
  *                      "delete"={"security"="is_granted('ROLE_GESTION_EQ')"}},
- *      order={"id" ="DESC"}
+ *      order={"id" ="DESC"},
+ *      attributes={"pagination_items_per_page" = 25}
+ * )
+ * @ApiFilter(
+ *  SearchFilter::class,
+ *      properties={"createdBy" : "partial"})
+ * @ApiFilter(
+ *  DateFilter::class, 
+ *      properties={"createdAt","moldingDay"}
  * )
  * @ORM\Entity(repositoryClass=MoldingRepository::class)
  */
